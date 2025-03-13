dcef2f05dc40f2a4595b9d5e4925541f75d9e581
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-13 20:04:14 +0600

    create some routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./pages/Auth/login";
+import SignUp from "./pages/Auth/signup";
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
 
 const App = () => {
   return (
-    <div className="text-center fontSize-2xl text-purple-500 mt-10 font-bold">
-      <h1>Expense Tracker</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}/>
+          <Route path="/login" exact element={<Login />}/>
+          <Route path="/signup" exact element={<SignUp />}/>
+          <Route path="/dashboard" exact element={<Home />}/>
+          <Route path="/income" exact element={<Income />}/>
+          <Route path="/expense" exact element={<Expense />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
