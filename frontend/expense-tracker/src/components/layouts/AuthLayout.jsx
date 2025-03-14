37cf524c0fe63eb595053257a619d2afb1349501
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-14 12:46:53 +0600

    add the income-expense image

--- frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import expenses from '../../assets/images/expenses.png'
+import incomeExpense from '../../assets/images/income_expenses.png'
 
 const AuthLayout = ({children}) => {
   return (
@@ -9,12 +9,12 @@ const AuthLayout = ({children}) => {
             {children}
         </div>
 
-        <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
-            <div className=""></div>
-            <div className=""></div>
-            <div className=""></div>
+        <div className="hidden md:block w-[50vw] h-screen bg-violet-300 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+            <div className="w-48 h-48 rounded-[40px] bg-blue-500 absolute -top-7 -left-5"></div>
+            <div className="w-48 h-56 rounded-[40px] border-[20px] border-violet-600 absolute top-[25%] -right-10"></div>
+            <div className="w-48 h-48 rounded-[40px] bg-blue-400 absolute -bottom-10 -left-5"></div>
 
-            <img src={expenses} className=''
+            <img src={incomeExpense} className='w-64 lg:w-[90%] h-105 absolute bottom-12 rounded shadow-lg shadow-blue-400/15' alt="income-expense"
             />
         </div>
     </div>
